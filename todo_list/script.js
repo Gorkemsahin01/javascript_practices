287d574ea0a03f70eb2132a4f6d6878104195e92
gorkemsahin <gorkemsahin012@gmail.com>
2023-10-27 16:14:18 +0300

    todo listeme javascript özellikleri ekledim

--- todo_list/script.js
+++ todo_list/script.js
@@ -1,4 +1,45 @@
 const inputBox= document.getElementById("input-box");
 const listConteiner=document.getElementById("list-conteiner");
 
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listConteiner.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+       
+    }
+    inputBox.value ='';
+}
 
+listConteiner.addEventListener("click", function(e){
+
+   if(e.target.tagName ==="LI"){
+    e.target.classList.toggle("checked");
+    saveData();
+
+   }
+
+   else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+    
+    
+   
+   }
+
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listConteiner.innerHTML);
+}
+
+function showTask(){
+    listConteiner.innerHTML =localStorage.getItem("data");
+}
+showTask();
